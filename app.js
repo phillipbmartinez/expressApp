78dae105038ad6ea5aa54bcb6c13408b743a908d
Phillip Martinez <phillip.martinez210@gmail.com>
2024-09-09 08:28:51 -0500

    Added route for root path for a GET request.

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const PORT = 8080;
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("GET request")
+})
+
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
 })
